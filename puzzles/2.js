3afd60a6a77688736d3e1ab058d1991be76ce696
Colton Dibbs <colton.dibbs@thrivent.com>
2023-12-03 09:26:21 -0600

    Day 2

---
+++ puzzles/2.js
@@ -0,0 +1,49 @@
+import { readFileLines } from "../utils/input.js";
+
+const parseInputData = () => {
+  const games = readFileLines("input/2.txt");
+  return games.map((game) => {
+    const [gameNumber, grabs] = game.split(": ");
+    const gameId = Number(gameNumber.split(" ")[1]);
+    const maxColors = grabs.split("; ").reduce(
+      (prev, grab) => {
+        const revealed = grab.split(", ");
+        revealed.forEach((pieceColor) => {
+          const [count, color] = pieceColor.split(" ");
+          const numericCount = Number(count);
+          if (numericCount > prev[color]) {
+            prev[color] = numericCount;
+          }
+        });
+        return prev;
+      },
+      { red: 0, blue: 0, green: 0 }
+    );
+
+    return { id: gameId, maxColors };
+  });
+};
+
+const solve = () => {
+  const MAX_RED = 12;
+  const MAX_GREEN = 13;
+  const MAX_BLUE = 14;
+
+  const gameData = parseInputData();
+  const result = gameData.reduce(
+    (prev, current) => {
+      const { red, green, blue } = current.maxColors;
+      if (!(red > MAX_RED || green > MAX_GREEN || blue > MAX_BLUE)) {
+        prev.total += current.id;
+      }
+      prev.power += red * green * blue;
+      return prev;
+    },
+    { total: 0, power: 0 }
+  );
+
+  console.log(`The total is ${result.total}`);
+  console.log(`The total power is ${result.power}`);
+};
+
+solve();
